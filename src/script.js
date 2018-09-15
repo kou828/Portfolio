6862e4fe00462df5317ff606aac17964e4c55be6
kou <kouken_volleyball@yahoo.co.jp>
2018-09-15 18:11:38 +0900

    vuetest

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
 var app = new Vue({
     el: '#app',
+    
     data: {
-        message: 'Hello Vue!'
+        seen:true,
+        todos:[
+            { text: '1' },
+            { text: '2' },
+            { text: '3' }
+        ]
+    }
+     ,methods: {
+        reverseMessage: function () {
+            this.todos.push({text:"222"});
+        }
     }
 })
