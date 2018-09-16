c076bb14851e7cabfcceb2144f1ae2b2b49ef36a
kou <kouken_volleyball@yahoo.co.jp>
2018-09-16 22:24:15 +0900

    Vue.js の基本を学習

--- src/script.js
+++ src/script.js
@@ -15,3 +15,29 @@ var app = new Vue({
         }
     }
 })
+
+var app2 = new Vue({
+    el: '#app2',
+
+    data: {
+        message: 'hoge',
+    }
+})
+
+// todo-item と呼ばれる新しいコンポーネントを定義
+Vue.component('todo-item', {
+    props:['todo']
+    , template: '<li>{{ todo.text }}</li>'
+})
+
+// コンポーネント呼び出し
+var app3 = new Vue({
+    el: '#app3'
+    ,data: {
+        groceryList: [ 
+            {id : 0, text: 'aaa'}
+            , { id: 3, text: 'vvv' }
+            , { id: 2, text: 'ccc' }
+        ]
+    }
+})
