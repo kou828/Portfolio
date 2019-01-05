dfac3dbb94006b615b1779b66e2d9b0f257b9f57
kou <kouken_volleyball@yahoo.co.jp>
2019-01-05 19:47:35 +0900

    「games」の情報を追加

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
-    //$("#info").hide("blind",'',700);
+
+    infoButtonClickEvent();
+
+    $("#infoButton").click(function(){infoButtonClickEvent();});
+    $("#gamesButton").click(function(){gamesButtonClickEvent();});
+
+    $("#mainArea").css("display", "");
+
 });
 
+function infoButtonClickEvent() {
+    $("#info").show("blind",'',700);
+    $("#games").hide("blind",'',700);
+}
+
+function gamesButtonClickEvent() {
+    $("#info").hide("blind",'',700);
+    $("#games").show("blind",'',700);
+}
+
+function productsButtonClickEvent() {
+
+}
